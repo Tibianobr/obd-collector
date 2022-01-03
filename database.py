59f94a275e549cd02bc19ab9afd5169934343946
Brenno Serrato <brenno.serrato@ubivis.io>
2022-01-03 18:45:24 -0300

    database implementado

---
+++ database.py
@@ -0,0 +1,33 @@
+import sqlite3 as sql
+import datetime
+
+
+class Database:
+    __CREATE_TABLE_DATA__ = r'CREATE TABLE data(timestamp text, key text, value text);'
+    __INSERT_VALUES_SQL__ = r"INSERT INTO data (timestamp,key,value) VALUES ('{timestamp}','{key}','{value}')"
+    __MIGRATION__ = [__CREATE_TABLE_DATA__]
+
+    def __init__(self):
+        self.connection = self.get_connection()
+        try:
+            self.__init_db__()
+        except:
+            print('DB JA INICIADO')
+
+    def get_connection(self):
+        return sql.connect('test.db')
+
+    def get_cursor(self):
+        return self.connection.cursor()
+
+    def __init_db__(self):
+        for script in self.__MIGRATION__:
+            self.get_cursor().execute(script)
+            self.connection.commit()
+
+    def insert_values(self, key, value):
+        date = datetime.datetime.now()
+        statement = self.__INSERT_VALUES_SQL__.format(timestamp=str(date), key=key, value=value)
+        print(statement)
+        self.get_cursor().execute(statement)
+        self.connection.commit()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-## pip list | findstr /I obd
-## Downgrade obd to v0.6.1
-## Trocar async pra asyncn (arquivo e imports na lib python - C:\Users\UbivisNb05\AppData\Local\Programs\Python\Python37\Lib\site-packages\obd)
-
-## LINK PARA COMANDOS OBD - https://python-obd.readthedocs.io/en/latest/Command%20Tables/
+# ## pip list | findstr /I obd
+# ## Downgrade obd to v0.6.1
+# ## Trocar async pra asyncn (arquivo e imports na lib python - C:\Users\UbivisNb05\AppData\Local\Programs\Python\Python37\Lib\site-packages\obd)
+#
+# ## LINK PARA COMANDOS OBD - https://python-obd.readthedocs.io/en/latest/Command%20Tables/
 import obd
 
 # Ativa DEBUGGER do OBD
@@ -13,38 +13,53 @@ ports = obd.scan_serial()
 print("Portas: ")
 print(ports)
 
-connection = obd.OBD()
+connection = obd.OBD(portstr=ports[0])
 
 # Print supported commands
 commands = connection.supported_commands
 print("Supported commands: ")
 for command in commands:
-  print(command.name)
+    print(command.name)
 
 # Send a command
 while True:
-  command = input("Enter command (type 'quit' to exit): ")
-  if (command == "quit"):
-    break
-  try:
-    response = connection.query(obd.commands[command])
-    print(response.value)
-  except Exception as ex:
-    print("Error: " + str(ex))
+    command = input("Enter command (type 'quit' to exit): ")
+    if command == "quit":
+        break
+    try:
+        response = connection.query(obd.commands[command])
+        print(response.value)
+    except Exception as ex:
+        print("Error: " + str(ex))
 
 # Close the connection
 connection.close()
+#
+# #####################################################
+#
 
-#####################################################
+# # cmd = obd.commands.FUEL_LEVEL
+# # response = connection.query(cmd)
+# # print(reponse.value)
+#
+# # cmd = obd.commands.RPM
+# # response = connection.query(cmd)
+# # print(reponse.value)
+#
+# # cmd = obd.commands.SPEED
+# # response = connection.query(cmd)
+# # print(reponse.value)
 
-#cmd = obd.commands.FUEL_LEVEL
-#response = connection.query(cmd)
-#print(reponse.value)
 
-#cmd = obd.commands.RPM
-#response = connection.query(cmd)
-#print(reponse.value)
+###########################
+# USANDO O BANCO DE DADOS #
+###########################
 
-#cmd = obd.commands.SPEED
-#response = connection.query(cmd)
-#print(reponse.value)
+
+# from database import Database
+# import random
+#
+# db = Database()
+#
+# while True:
+#     db.insert_values(key, value)
